b7ba137cb89939f045d677fe7da6d8db3be43f98
Leeh Peter <leeh123peter@gmail.com>
2021-07-05 11:22:26 +0200

    tooltip on post create

--- src/components/AutoResizeTextArea.tsx
+++ src/components/AutoResizeTextArea.tsx
@@ -1,18 +1,27 @@
-import { Textarea } from "@chakra-ui/react";
+import { Textarea, Tooltip } from "@chakra-ui/react";
 import ResizeTextarea from "react-textarea-autosize";
 import React from "react";
 
 export const AutoResizeTextArea = React.forwardRef((props, ref) => {
   return (
-    <Textarea
-      minH="unset"
-      overflow="hidden"
-      w="100%"
-      resize="none"
-      ref={ref as any}
-      minRows={1}
-      as={ResizeTextarea}
-      {...props}
-    />
+    <Tooltip
+      hasArrow
+      label="LaTex expressions can be inserted inside double dollar signs, e.g. $\bold{F}$. Markdown syntax is also supported. For example, insert a code snippet enclosed in two backticks:
+      `print('Hello, World!')`"
+      bg="red.600"
+      placement="bottom-end"
+      IsOpen
+    >
+      <Textarea
+        minH="unset"
+        overflow="hidden"
+        w="100%"
+        resize="none"
+        ref={ref as any}
+        minRows={1}
+        as={ResizeTextarea}
+        {...props}
+      />
+    </Tooltip>
   );
 });
