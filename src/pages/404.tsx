f0f2a601849f9cef4484a88049663b59dea0bf68
Leeh Peter <leeh123peter@gmail.com>
2021-08-12 10:09:38 +0200

    custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import { Layout } from "../components/Layout";
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <Box>
+        Unless you have a time machine, this content is no longer available.
+      </Box>
+    </Layout>
+  );
+}
