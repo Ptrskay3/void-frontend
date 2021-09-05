f1004299d5bb4ac6b36dbe8b56aad33c7465ba0a
Leeh Peter <leeh123peter@gmail.com>
2021-09-05 12:55:27 +0200

    fix build error

--- src/pages/messaging.tsx
+++ src/pages/messaging.tsx
@@ -1,14 +1,11 @@
 import { withUrqlClient } from "next-urql";
 import React from "react";
-import { useNewMessageSubscription } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
 interface messagingProps {}
 
 // TODO
 const Messaging: React.FC<messagingProps> = ({}) => {
-  const [{ data }] = useNewMessageSubscription();
-
   return <div>Not implemented</div>;
 };
 
